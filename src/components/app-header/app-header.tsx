e41130acab492bab14d515fe776cfad6bff226d1
Kupriyanova Natalia <kupriyanova@morizo.ru>
2025-04-07 02:18:24 +0300

    Add modal redirect

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -6,26 +6,46 @@ import {
 	Logo,
 	ProfileIcon,
 } from '@ya.praktikum/react-developer-burger-ui-components';
+import { NavLink } from 'react-router-dom';
+import burgerIcon from '../../images/burgerIcon.svg';
 
 export const AppHeader = ({}) => {
 	return (
 		<header className={s.header}>
 			<nav className={s.header__inner}>
 				<div className={s.header__buttons}>
-					<a className={`${s.link__active} ${s.link} `} href='/'>
+					{/*<a className={`${s.link__active} ${s.link} `} href='/'>*/}
+					{/*	<BurgerIcon type='primary' className={s.link_logo} />*/}
+					{/*	Конструктор*/}
+					{/*</a>*/}
+
+					<NavLink
+						className={({ isActive }) =>
+							isActive ? `${s.link__active} ${s.link}` : `${s.link}`
+						}
+						to='/'>
 						<BurgerIcon type='primary' className={s.link_logo} />
 						Конструктор
-					</a>
-					<a className={s.link} href='/'>
-						<ListIcon type='secondary' className={s.link_logo} /> Лента заказов
-					</a>
+					</NavLink>
+					<NavLink
+						className={({ isActive }) =>
+							isActive ? `${s.link__active} ${s.link}` : `${s.link}`
+						}
+						to='/orders'>
+						<ListIcon type='secondary' className={s.link_logo} />
+						Лента заказов
+					</NavLink>
 				</div>
 				<Logo className={s.logo} />
 				<div className={s.header__buttons}>
-					<a className={s.link} href='/'>
+					<NavLink
+						className={({ isActive }) =>
+							isActive ? `${s.link__active} ${s.link}` : `${s.link}`
+						}
+						to='/profile'>
 						<ProfileIcon type='secondary' className={s.link_logo} /> Личный
 						кабинет
-					</a>
+					</NavLink>
 				</div>
 			</nav>
 		</header>
