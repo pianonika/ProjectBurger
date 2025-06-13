b100475993d39c7e3b06e63dc5d2fdf496246e26
Kupriyanova Natalia <pianonika@github.com>
2025-06-14 01:21:46 +0300

    change repo

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,7 +9,7 @@ const production = process.env.NODE_ENV === 'production';
 module.exports = {
 	entry: path.resolve(__dirname, '..', './src/index.tsx'), //точка входа в наше приложение содержит абсолютный путь к index.ts
 	output: {
-		path: path.resolve(__dirname, '..', './dist'), //путь куда будет собираться наш проект
+		path: path.resolve(__dirname, '..', './build'), //путь куда будет собираться наш проект
 		filename: production
 			? 'static/scripts/[name].[contenthash].js'
 			: 'static/scripts/[name].js', // имя нашего бандла
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = {
 	devtool: 'eval-source-map',
 	devServer: {
 		historyApiFallback: true,
-		static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+		static: path.resolve(__dirname, './build'), // путь, куда "смотрит" режим разработчика
 		// compress: true, // это ускорит загрузку в режиме разработки
 		port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
 		open: true, // сайт будет открываться сам при запуске npm run dev
